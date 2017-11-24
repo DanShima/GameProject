53edad2c90cb16f54428ce6e65ed6a08794b9d81
Dan <danningshimanovskiy@gmail.com>
2017-11-24 10:46:06 +0100

    new arrays
    ss

--- core/src/com/mygdx/game/Animator.java
+++ core/src/com/mygdx/game/Animator.java
@@ -16,11 +16,23 @@ public class Animator implements ApplicationListener {
     // Constant rows and columns of the sprite sheet
     private static final int FRAME_COLS = 3, FRAME_ROWS = 5;
 
-    private Animation<TextureRegion> walkAnimation; //animation object that animate the player
+    //animation object that animate the player
+    private Animation<TextureRegion> walkAnimationDOWN;
+    private Animation<TextureRegion> walkAnimationUP;
+    private Animation<TextureRegion> walkAnimationLEFT;
+    private Animation<TextureRegion> walkAnimationRIGHT;
+
     private Texture walkSheet; //sprite sheet for the player
     private SpriteBatch spriteBatch; //to draw on screen
     // A variable for tracking elapsed time for the animation (when the player moves)
     private float stateTime;
+
+    //movement arrays
+    TextureRegion[] walkFramesDOWN = new TextureRegion[FRAME_COLS];
+    TextureRegion[] walkFramesUP = new TextureRegion[FRAME_COLS];
+    TextureRegion[] walkFramesLEFT = new TextureRegion[FRAME_COLS];
+    TextureRegion[] walkFramesRIGHT = new TextureRegion[FRAME_COLS];
+
 
 
     //useless atm
@@ -39,44 +51,62 @@ public class Animator implements ApplicationListener {
                 walkSheet.getWidth() / FRAME_COLS,
                 walkSheet.getHeight() / FRAME_ROWS);
         //convert 2D array to normal array
-        //TODO this part for each type of animation (walkLeftFrames, walkRightFrames, idleFrames, etc.)
-        //TODO then put it in a switch statement.
-        TextureRegion[] walkFrames = new TextureRegion[FRAME_COLS];
+
+        //TODO put the movements in a switch statement.
+        walkFramesDOWN = new TextureRegion[FRAME_COLS];
+        walkFramesUP = new TextureRegion[FRAME_COLS];
+        walkFramesLEFT = new TextureRegion[FRAME_COLS];
+        walkFramesRIGHT = new TextureRegion[FRAME_COLS];
         //We only want to animate the three images in the first row
         //GO DOWN & IDLE
         for (int i = 0; i < FRAME_COLS; i++) {
-            walkFrames[i] = tmp[0][i];
+            walkFramesDOWN[i] = tmp[0][i];
         }
         //GO LEFT
         for (int i = 0; i < FRAME_COLS; i++) {
-            walkFrames[i] = tmp[1][i];
+            walkFramesLEFT[i] = tmp[1][i];
         }
         //GO RIGHT
         for (int i = 0; i < FRAME_COLS; i++) {
-            walkFrames[i] = tmp[2][i];
+            walkFramesRIGHT[i] = tmp[2][i];
         }
         //GO UP
         for (int i = 0; i < FRAME_COLS; i++) {
-            walkFrames[i] = tmp[3][i];
+            walkFramesUP[i] = tmp[3][i];
         }
-        // Initialize the Animation with the frame interval and array of frames
-        walkAnimation = new Animation<TextureRegion>(0.5f, walkFrames);
+
+        initializeAnimation(walkAnimationDOWN, walkFramesDOWN);
+        initializeAnimation(walkAnimationUP, walkFramesUP);
+        initializeAnimation(walkAnimationLEFT, walkFramesLEFT);
+        initializeAnimation(walkAnimationRIGHT, walkFramesRIGHT);
 
         // Instantiate a SpriteBatch for drawing and reset the elapsed animation time to 0
         spriteBatch = new SpriteBatch();
         stateTime = 0f;
 
     }
+    // Initialize the Animation with the frame interval and array of frames
+    public void initializeAnimation(Animation<TextureRegion> animation, TextureRegion[] walkFrames){
+        animation = new Animation<TextureRegion>(0.5f, walkFrames);
+    }
 
     //TODO make the player move
-    public void move(){}
+    public void move(float x, float y) {
+        boolean isTouched = true;
+
+        if (isTouched) {
+
+        }
+
+    }
 
     @Override
     public void render() {
         stateTime += Gdx.graphics.getDeltaTime(); // Accumulate elapsed animation time
         // Get current frame of animation for the current stateTime:
         // this method takes an elapsed time parameter and returns the appropriate image for that time. it loops through a series of images and do it again
-        TextureRegion currentFrame = walkAnimation.getKeyFrame(stateTime, true);
+        //TODO put the movements in a separate method
+        TextureRegion currentFrame = walkAnimationDOWN.getKeyFrame(stateTime, true);
         spriteBatch.begin();
         spriteBatch.draw(currentFrame, 0, 0); // Draw current frame at (0, 0)
         spriteBatch.end();
