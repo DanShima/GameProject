299be7acf4d746e1504753f28454aa12128fb26e
DanShima <giddy_with_panda@hotmail.com>
2017-11-29 22:30:51 +0100

    deleted some unused assets

--- android/assets/prototypegame-desert-128-v02.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="desert" tilewidth="128" tileheight="128" tilecount="312" columns="13">
- <image source="desert_128_v01.png" width="1664" height="3072"/>
-</tileset>
