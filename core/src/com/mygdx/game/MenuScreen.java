d397372809963820eaf442e001a5af2dca56cc24
KavithaSundarraj <jeyakumar.kavitha@gmail.com>
2017-12-05 15:03:03 +0100

    Changes made to include background

--- core/src/com/mygdx/game/MenuScreen.java
+++ core/src/com/mygdx/game/MenuScreen.java
@@ -5,7 +5,10 @@ package com.mygdx.game;
         import com.badlogic.gdx.ApplicationListener;
         import com.badlogic.gdx.Gdx;
         import com.badlogic.gdx.graphics.GL20;
+        import com.badlogic.gdx.graphics.Texture;
+        import com.badlogic.gdx.graphics.g2d.Sprite;
         import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+        import com.badlogic.gdx.graphics.g2d.TextureRegion;
         import com.badlogic.gdx.scenes.scene2d.InputEvent;
         import com.badlogic.gdx.scenes.scene2d.Stage;
         import com.badlogic.gdx.scenes.scene2d.ui.Skin;
@@ -14,15 +17,25 @@ package com.mygdx.game;
 
 
 
+
     public class MenuScreen implements ApplicationListener {
-    private TiledTest game;
     private SpriteBatch batch;
     private Skin skin;
     private Stage stage;
+
+    //private Texture backgroundTexture;
+    //private Sprite sprite;
+    //private TextureRegion region;
+
     @Override
     public void create() {
+        //backgroundTexture = new Texture(Gdx.files.internal("bakgroundmenu.png"));
+        //sprite =new Sprite(backgroundTexture,20,20,50,50);
+        //region=new TextureRegion(backgroundTexture,0,0,800,420);
+        //sprite=new Sprite(region);
+        //sprite.setSize(1,1);
+
         batch = new SpriteBatch();
-        game=new TiledTest();
         skin = new Skin(Gdx.files.internal(Constants.skin));
         stage = new Stage();
 
@@ -69,6 +82,11 @@ package com.mygdx.game;
         stage.addActor(exitButton);
         Gdx.input.setInputProcessor(stage);
     }
+
+    /*public void renderBackground()
+    {
+        sprite.draw(batch);
+    }*/
     @Override
     public void dispose() {
         batch.dispose();
@@ -78,7 +96,11 @@ package com.mygdx.game;
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
+        //batch.disableBlending();
+       // batch.draw(backgroundTexture,100,100);
+        //renderBackground();
         stage.draw();
+       // batch.enableBlending();
         batch.end();
     }
     @Override
@@ -90,5 +112,7 @@ package com.mygdx.game;
     @Override
     public void resume() {
     }
+
+
 }
 
