78f2cd9311b0925395f33e35161be4803c8c0ec6
osama alnajar <osama.alnajar82@gmail.com>
2017-12-06 23:14:51 +0100

    add score and health

--- core/src/com/mygdx/game/PlayerStats.java
+++ core/src/com/mygdx/game/PlayerStats.java
@@ -8,10 +8,40 @@ package com.mygdx.game;
 
 public class PlayerStats {
 
+    private String  name;
+   // private Player player;
+    private int score;
+    private int health;
+    private static int maxScore=75;
+
+
+
     int stepsTaken=0;
+    public PlayerStats(String name){
+        this.name=name;
+        score=75;
+        health=100;
+
+
+
+    }
+    public void setScore(int score){
+        this.score=score;
+        if(score > maxScore)
+            maxScore=score;
+
+    }
+    public void setHealth(int health){
+        this.health=health;
+
+    }
+
+
 
     public void addSteps(int steps){
         stepsTaken += steps;
+
+
     };
 
     public void resetSteps(int steps){
