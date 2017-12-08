e532c4feca4a86ce02559e6b62d50d63e38aaeb4
purbashaa <soni.kashish@gmail.com>
2017-12-08 12:01:16 +0100

    Splash Screen Created

--- core/src/com/mygdx/game/GearUp.java
+++ core/src/com/mygdx/game/GearUp.java
@@ -1,15 +1,36 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 
 /**
  * This class shows the player login page
  */
 
 public class GearUp extends Game {
+    public static int WIDTH;
+    public static int HEIGHT;
 
     @Override
     public void create() {
         this.setScreen(new PlayerLogin());
+        this.setScreen(new PlayerLogin());
+
+        WIDTH = Gdx.graphics.getWidth();
+        HEIGHT = Gdx.graphics.getHeight();
+
+        setScreen( new Splash(this) );
+    }
+    @Override
+    public void resize(int width, int height) {
+    }
+    @Override
+    public void pause() {
+    }
+    @Override
+    public void resume() {
+    }
+    @Override
+    public void dispose() {
     }
 }
