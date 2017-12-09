c76eea1bc6cc0443f54c3b5c6eb7dab485a2502f
KavithaSundarraj <jeyakumar.kavitha@gmail.com>
2017-12-09 19:36:43 +0100

    Firebase interface added to core

---
+++ core/src/com/mygdx/game/firebase.java
@@ -0,0 +1,11 @@
+package com.mygdx.game;
+
+/**
+ * Created by Shashidhar on 09-12-2017.
+ */
+
+public interface firebase {
+    public void onclicklogin(String name,String password);
+    public void onclicknewuser(String name,String password);
+
+}
